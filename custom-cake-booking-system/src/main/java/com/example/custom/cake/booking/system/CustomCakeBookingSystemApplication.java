af63c29d81dbecf5c391743cbf6368563d6499f7
AFKAR2003 <IT24101598@my.sliit.lk>
2025-05-18 15:23:11 +0530

    create project

---
+++ custom-cake-booking-system/src/main/java/com/example/custom/cake/booking/system/CustomCakeBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.custom.cake.booking.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomCakeBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomCakeBookingSystemApplication.class, args);
+	}
+
+}
---
+++ custom-cake-booking-system/src/test/java/com/example/custom/cake/booking/system/CustomCakeBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.custom.cake.booking.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomCakeBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
